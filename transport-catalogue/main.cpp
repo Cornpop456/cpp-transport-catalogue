1cc5f21946d3775dd2180a12308afca340d325a3
Victor Ginzburg <v-viktor456@mail.ru>
2022-06-12 19:59:22 +0300

    add log_duration

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 
 #include "catalogue_client.h"
+#include "log_duration.h"
 
 using namespace std;
 using namespace transport;
 
 int main() {    
     TransportCatalogue catalogue;
+    {
+    LOG_DURATION_STREAM("fill"s, cerr);
     client::FillCatalogue(catalogue, cin);
+    }
 
+    {
+    LOG_DURATION_STREAM("read"s, cerr);
     client::ProcessQueries(catalogue, cin, cout);
+    }
 
     return 0;
 }
